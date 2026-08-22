9d7c93dc6f4f707ff465778ebb057e14399d2012
Arumugam28 <arumugajai447@gmail.com>
2026-08-22 19:38:39 +0530

    Refactoring Time : Label  #136

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,40 +1,32 @@
 <x-layout>
-  <h1 class="my-16 text-center text-4xl font-medium text-slate-600">
-    Sign in to your account
-  </h1>
+  <x-breadcrumbs class="mb-4"
+    :links="[
+        'Jobs' => route('jobs.index'),
+        $job->title => route('jobs.show', $job),
+        'Apply' => '#',
+    ]" />
 
-  <x-card class="py-8 px-16">
-    <form action="{{ route('auth.store') }}" method="POST">
+  <x-job-card :$job />
+
+  <x-card>
+    <h2 class="mb-4 text-lg font-medium">
+      Your Job Application
+    </h2>
+
+    <form action="{{ route('job.application.store', $job) }}" method="POST"
+      enctype="multipart/form-data">
       @csrf
-
-      <div class="mb-8">
-        <x-label for="email" :required="true">E-mail</x-label>
-        <x-text-input name="email" />
+      <div class="mb-4">
+        <x-label for="expected_salary" :required="true">Expected Salary</x-label>
+        <x-text-input type="number" name="expected_salary" />
       </div>
 
-      <div class="mb-8">
-        <x-label for="password" :required="true">
-          Password
-        </x-label>
-        <x-text-input name="password" type="password" />
+      <div class="mb-4">
+        <x-label for="cv" :required="true">Upload CV</x-label>
+        <x-text-input type="file" name="cv" />
       </div>
 
-      <div class="mb-8 flex justify-between text-sm font-medium">
-        <div>
-          <div class="flex items-center space-x-2">
-            <input type="checkbox" name="remember"
-              class="rounded-sm border border-slate-400">
-            <label for="remember">Remember me</label>
-          </div>
-        </div>
-        <div>
-          <a href="#" class="text-indigo-600 hover:underline">
-            Forget password?
-          </a>
-        </div>
-      </div>
-
-      <x-button class="w-full bg-green-50">Login</x-button>
+      <x-button class="w-full">Apply</x-button>
     </form>
   </x-card>
 </x-layout>
--- resources/views/components/lable.blade.php
+++ resources/views/components/lable.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
-</div>
+<label class="mb-2 block text-sm font-medium text-slate-900"
+  for="{{ $for }}">
+  {{ $slot }} @if ($required)
+    <span>*</span>
+  @endif
+</label>
